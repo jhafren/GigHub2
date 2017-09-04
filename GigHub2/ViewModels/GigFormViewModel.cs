a79c105713d8dcef6d5c5d5cd1d3fcfb1ad3c6cb
haffe <haffe@LAPTOP-MTHRJET2>
2017-09-04 21:23:20 +0300

    Implemented validation.

--- GigHub2/ViewModels/GigFormViewModel.cs
+++ GigHub2/ViewModels/GigFormViewModel.cs
@@ -1,19 +1,30 @@
 ﻿using GigHub2.Models;
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub2.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
-        public DateTime DateTime
+        public DateTime GetDateTime()
         {
-            get { return DateTime.Parse(string.Format("{0} {1}", Date, Time)); }
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
         }
     }
 }
