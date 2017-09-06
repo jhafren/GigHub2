dab8570b60557686c86854366548998584efc075
haffe <haffe@LAPTOP-MTHRJET2>
2017-09-06 21:24:05 +0300

    Improved the look and feel of the home page.

--- GigHub2/Controllers/HomeController.cs
+++ GigHub2/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
 ﻿using GigHub2.Models;
 using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GigHub2.Controllers
@@ -21,6 +19,7 @@ namespace GigHub2.Controllers
         {
             var upcomingGigs = _context.Gigs
                 .Include(g => g.Artist)
+                .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
 
             return View(upcomingGigs);
