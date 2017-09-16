2661436c466450ddecddd8e956dbd19d5537a87b
haffe <haffe@LAPTOP-MTHRJET2>
2017-09-16 17:34:04 +0300

    Implemented View Gigs I'm Attending

--- GigHub2/Controllers/GigsController.cs
+++ GigHub2/Controllers/GigsController.cs
@@ -1,7 +1,7 @@
 ﻿using GigHub2.Models;
 using GigHub2.ViewModels;
 using Microsoft.AspNet.Identity;
-using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -14,6 +14,27 @@ namespace GigHub2.Controllers
         public GigsController()
         {
             _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Attending()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Attendances
+                .Where(a => a.AttendeeId == userId)
+                .Select(a => a.Gig)
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .ToList();
+
+            var viewModel = new GigsViewModel
+            {
+                UpcomingGigs = gigs,
+                ShowActions = User.Identity.IsAuthenticated,
+                Heading = "Gigs I'm Attending"
+            };
+
+            return View("Gigs", viewModel);
         }
 
         [Authorize]
---
+++ GigHub2/ViewModels/GigsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GigHub2.Models;
+using System.Collections.Generic;
+
+namespace GigHub2.ViewModels
+{
+    public class GigsViewModel
+    {
+        public IEnumerable<Gig> UpcomingGigs { get; set; }
+        public bool ShowActions { get; set; }
+        public string Heading { get; set; }
+    }
+}
