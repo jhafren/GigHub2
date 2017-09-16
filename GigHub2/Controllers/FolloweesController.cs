d20b8666ec1425877d25b575a15e1bac6d161e8b
haffe <haffe@LAPTOP-MTHRJET2>
2017-09-16 18:42:47 +0300

    Added Artists Im following page

---
+++ GigHub2/Controllers/FolloweesController.cs
@@ -0,0 +1,29 @@
+﻿using GigHub2.Models;
+using Microsoft.AspNet.Identity;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub2.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Followees()
+        {
+            var userId = User.Identity.GetUserId();
+            var followees = _context.Followings
+                .Where(a => a.FollowerId == userId)
+                .Select(a => a.Followee)
+                .ToList();
+
+            return View(followees);
+        }
+    }
+}
