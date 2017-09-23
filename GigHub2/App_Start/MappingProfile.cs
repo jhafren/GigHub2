99bebfc6d0f5bdcb5b6d0ee0bac4472ec4e46b90
haffe <haffe@LAPTOP-MTHRJET2>
2017-09-23 16:30:44 +0300

    Added Notifications api

---
+++ GigHub2/App_Start/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GigHub2.Controllers.Api;
+using GigHub2.Dtos;
+using GigHub2.Models;
+
+namespace GigHub2.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.Initialize(c => c.CreateMap<ApplicationUser, UserDto>());
+            Mapper.Initialize(c => c.CreateMap<Gig, GenreDto>());
+            Mapper.Initialize(c => c.CreateMap<Notification, NotificationDto>());
+        }
+    }
+}
