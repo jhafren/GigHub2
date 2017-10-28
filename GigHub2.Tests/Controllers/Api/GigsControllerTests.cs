12d8957f30b182a4de04bcad90e66a4314fa9502
haffe <haffe@LAPTOP-MTHRJET2>
2017-10-28 13:31:38 +0300

    Added unit test project and a test for cancel method in GigsController api.

---
+++ GigHub2.Tests/Controllers/Api/GigsControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using GigHub2.Controllers.Api;
+using GigHub2.Core;
+using GigHub2.Core.Repositories;
+using GigHub2.Tests.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Web.Http.Results;
+
+namespace GigHub2.Tests.Controllers.Api
+{
+    [TestClass]
+    public class GigsControllerTests
+    {
+        private GigsController _controller;
+
+        public GigsControllerTests()
+        {
+            var mockRepository = new Mock<IGigRepository>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(m => m.Gigs).Returns(mockRepository.Object);
+
+            _controller = new GigsController(mockUnitOfWork.Object);
+            _controller.MockCurrentUser("1", "user1@domain.com");
+        }
+
+        [TestMethod]
+        public void Cancel_NoGigWithGivenIdExists_ShouldReturnNotFound()
+        {
+            var result = _controller.Cancel(1);
+
+            result.Should().BeOfType<NotFoundResult>();
+        }
+    }
+}
---
+++ GigHub2.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GigHub2.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GigHub2.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e3620dab-c0fd-448b-aabc-d5b959fa6ed4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
