c7eb4bfd2a321f9e40dfa7f3f1a1270e2020f923
haffe <haffe@LAPTOP-MTHRJET2>
2017-10-28 12:41:13 +0300

    Enabled Ninject for web api controllers

--- GigHub2/App_Start/NinjectWebCommon.cs
+++ GigHub2/App_Start/NinjectWebCommon.cs
@@ -3,29 +3,29 @@
 
 namespace GigHub2.App_Start
 {
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+    using Ninject;
+    using Ninject.Extensions.Conventions;
+    using Ninject.Web.Common;
+    using Ninject.Web.WebApi;
     using System;
     using System.Web;
+    using System.Web.Http;
 
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
-    using Ninject;
-    using Ninject.Web.Common;
-    using Ninject.Extensions.Conventions;
-
-    public static class NinjectWebCommon 
+    public static class NinjectWebCommon
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
@@ -33,7 +33,7 @@ namespace GigHub2.App_Start
         {
             bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
@@ -51,9 +51,11 @@ namespace GigHub2.App_Start
                 kernel.Bind(x =>
                 {
                     x.FromThisAssembly()
-                     .SelectAllClasses() 
+                     .SelectAllClasses()
                      .BindDefaultInterface();
                 });
+
+                GlobalConfiguration.Configuration.DependencyResolver = new NinjectDependencyResolver(kernel);
 
                 return kernel;
             }
@@ -70,6 +72,6 @@ namespace GigHub2.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-        }        
+        }
     }
 }
